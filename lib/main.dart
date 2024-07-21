fac22facc49493311f2dc03c1d8c5af948c26c2c
Ahrend <caguilar@agrokasa.com.pe>
2024-07-21 11:54:25 -0500

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:providerapp/home_page.dart';
+import 'package:providerapp/provider/animal_provider.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(ProviderExample());
+}
+
+class ProviderExample extends StatelessWidget {
+  const ProviderExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (context)=> PerritoProvider(),
+        )
+    ],
+    child: MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+    );
+  }
 }
